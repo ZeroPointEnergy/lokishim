5f79c3ef271af80c5da0095cd8be3a8a9154299f
Andreas Zuber <zuber@puzzle.ch>
2020-01-24 23:30:29 +0100

    Add two more missing functions
    Game seems playable now

--- smacshim.c
+++ smacshim.c
@@ -3,6 +3,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void *__builtin_new(size_t size)
+{
+    return malloc(size);
+}
+
+void __builtin_delete(void *ptr)
+{
+    free(ptr);
+}
+
 void *__builtin_vec_new(size_t size)
 {
     return malloc(size);
